a81bc45072db53b9a0d3bcaced265af54ad787fc
vanderL <vnd.vander@gmail.com>
2021-07-04 20:28:42 -0300

    Boolean part I

---
+++ JAVASCRIPT/boolean-e-condicionais-i/script.js
@@ -0,0 +1,25 @@
+var possuiGraduacao = true;
+var possuiDoutorado = true;
+
+if (possuiGraduacao) {
+    console.log("É verdadeiro")
+} else if (possuiDoutorado) {
+    console.log("É possui doutorado")
+} else {
+    console.log("É possui nada")
+}
+
+var nome = ''
+
+if(nome){
+    console.log(nome)
+} else {
+    console.log('Nome não existe');
+}
+
+if(!possuiGraduacao){
+    console.log(possuiGraduacao);
+    console.log('Não possui graduacao');
+}
+
+var x = 10;
